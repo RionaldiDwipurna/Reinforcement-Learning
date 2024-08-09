649bf4f6a4f97e22139e4cd0af16ad5849b1dd7e
rionaldi <rionaldi.dwipurna@gmail.com>
2024-08-09 11:39:58 +0700

    fix

--- DQN.py
+++ DQN.py
@@ -11,9 +11,9 @@ import pickle
 class DQN(nn.Module):
     def __init__(self, action_shape, state_shape):
         super().__init__()
-        self.fc1 = nn.Linear(num_states, 64)
+        self.fc1 = nn.Linear(state_shape, 64)
         self.fc2 = nn.Linear(64, 64)
-        self.out = nn.Linear(64, num_actions)
+        self.out = nn.Linear(64, action_shape)
         pass
 
     def forward(self, x):
