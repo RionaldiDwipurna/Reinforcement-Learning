23b5b255f5c72df18ae816d1d993f0a5340c3d1f
rionaldi <rionaldi.dwipurna@gmail.com>
2024-08-12 16:27:10 +0700

    testing

--- DQN.py
+++ DQN.py
@@ -15,14 +15,16 @@ import pickle
 class DQN(nn.Module):
     def __init__(self, action_shape, state_shape):
         super().__init__()
-        self.fc1 = nn.Linear(state_shape, 64)
-        self.fc2 = nn.Linear(64, 64)
+        self.fc1 = nn.Linear(state_shape, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 64)
         self.out = nn.Linear(64, action_shape)
         pass
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         x = self.out(x)  
         return x
 
